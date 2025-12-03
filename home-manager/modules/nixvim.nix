ca861a804ed72d39747c1707d1850d41acf6f118
Janmejay Singh <singh.janmejay@gmail.com>
2025-12-03 15:56:46 +0530

    Improve aerospace term-launch and tmux paste bind fix + nvim C-y confirm

--- home-manager/modules/nixvim.nix
+++ home-manager/modules/nixvim.nix
@@ -139,25 +139,8 @@ in
 	      }
 	      ];
 	      mapping = {
-		"<CR>" = "cmp.mapping.confirm({ select = true})";
-# "<Tab>" = {
-#   action = ''
-#     function(fallback)
-#       if cmp.visible() then
-#         cmp.select_next_item()
-#       elseif luasnip.expandable() then
-#         luasnip.expand()
-#       elseif luasnip.expand_or_jumpable() then
-#         luasnip.expand_or_jump()
-#       elseif check_backspace() then
-#         fallback()
-#       else
-#         fallback()
-#       end
-#     end
-#   '';
-#   modes = [ "i" "s" ];
-# };
+		"<C-y>" = "cmp.mapping.confirm({ select = true})";
+	        "<C-e>" = "cmp.mapping.abort()";
 	      };
 	    };
 	  };
