3b676f7ad9b5508d285b15ba17b24e22923bd5a3
Janmejay Singh <singh.janmejay@gmail.com>
2026-03-20 20:22:18 +0530

    Fix Nvim <-> LSP-server calls for symlinked paths

--- home-manager/modules/nixvim.nix
+++ home-manager/modules/nixvim.nix
@@ -33,6 +33,8 @@ in{
   };
 
   config = lib.mkIf cfg.enable {
+    home.file.".config/sym_xlate.nvim.lua".source = ../../dots/sym_xlate.nvim.lua;
+
     programs.nixvim = {
       config = {
         nixpkgs.pkgs = pkgs;
@@ -66,6 +68,7 @@ in{
               },
             },
           })
+          vim.cmd('source ~/.config/sym_xlate.nvim.lua')
         '';
         keymaps = [
           {
