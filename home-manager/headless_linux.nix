828525b2672ffc5948520013fed4ecb2de6df28c
Janmejay Singh <janmejay.singh@sentinelone.com>
2026-07-11 00:21:43 +0530

    Fix build-vm shell to show env-name

--- home-manager/headless_linux.nix
+++ home-manager/headless_linux.nix
@@ -1,3 +1,4 @@
+{ lib, ... }:
 {
   home.stateVersion = "24.05";
 
@@ -7,4 +8,15 @@
   nixvim.enable = true;
 
   programs.zsh.oh-my-zsh.theme = "sunaku";
+
+  # sunaku has no agnoster-style prompt_context hook, so prepend a segment showing
+  # the nix-shell name + nesting depth, mirroring the host agnoster override in
+  # shared.nix. $name is set by `nix develop` (e.g. "s1-env"), empty in a plain
+  # nested shell; ${name%%,*} strips any trailing metadata like the sed there does.
+  # mkAfter runs this after oh-my-zsh has loaded the theme and set PROMPT.
+  programs.zsh.initContent = lib.mkAfter ''
+    if (( SHLVL > 1 )); then
+      PROMPT="%K{white}%F{black} ''${name%%,*}/''${SHLVL} %f%k''${PROMPT}"
+    fi
+  '';
 }
